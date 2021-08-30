be6d4daa173b203c3eb0627f01f29c28347047e1
roshdy <mahmoud.roshdy@g2k-group.com>
2021-08-30 02:30:18 +0200

    update

--- run_docker.sh
+++ run_docker.sh
@@ -3,9 +3,9 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-docker build --tag=project-ml:0.0.1 .
+docker build --tag=roshdy/udacitymlpro .
 # Step 2: 
 docker images
 
 # Step 3: 
-docker run -d -p 8000:80  project-ml:0.0.1 
+docker run -d -p 8000:80  roshdy/udacitymlpro
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath=roshdy/udacitymlpro
 
 # Step 2
-# Run the Docker Hub container with kubernetes
+kubectl  run devops-ml-prediction --image=$dockerpath 
 
 
 # Step 3:
-# List kubernetes pods
+kubectl  get pods --all-namespaces
 
 # Step 4:
-# Forward the container port to a host
+kubectl port-forward pod/devops-ml-prediction 8000:80
 
